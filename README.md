b16917ab0457838d26fd82de683472e860c33ec0
naut92 <naut92@gmail.com>
2022-07-26 17:38:10 +0200

    docker

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 
+#Java 11, #Spring Boot, #AOP, #CompletableFuture, #Async, #docker-compose, #custom annotation
+
+
+Spring-boot application. Returns HTTP 200 and empty body. Limits the number of requests from one IP address to N times per X minutes/seconds.
+If the number of requests is greater, then it returns a 502 error code, until the number of requests for the specified interval falls below N.
+
 - endpoint: http://localhost:8081/api/ip
 
 Docker image:
--- src/test/java/com/retry_connection/RetryConnectionApplicationTests.java
+++ src/test/java/com/retry_connection/RetryConnectionApplicationTests.java
@@ -8,11 +8,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@WebMvcTest(Controller.class)
+@WebMvcTest(Controller.class)
 public class RetryConnectionApplicationTests {
 
 	@Autowired
@@ -21,9 +22,11 @@ public class RetryConnectionApplicationTests {
 	@MockBean
 	private RetryConnectionService service;
 
-	//@Test
+	@Test
 	public void shouldReturnTrueFromService() throws Exception {
-		//when(service.getClientIp("62.4.43.31".getBytes(StandardCharsets.UTF_8))).thenReturn(true);
-		this.mockMvc.perform(get("/ip")).andDo(print()).andExpect(status().isOk());
+		for (int i = 0; i < 6; i++) {
+			when(service.someMethod1("")).thenReturn(true);
+			this.mockMvc.perform(get("/ip")).andDo(print()).andExpect(status().isOk());
+		}
 	}
 }
